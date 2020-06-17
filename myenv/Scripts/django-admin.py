eacc332b74c8ed669af60533c0e4d694675cd50e
tayyabaolakh <tayyabaolakh@users.noreply.github.com>
2020-06-17 15:51:29 +0500

    First Commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\hp\Desktop\online-movie-ticket-booking-system-master\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
